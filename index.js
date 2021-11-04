1bed661c9ac71f743839e3b94363b0465a866344
milleseseau <leojimlavi@gmail.com>
2021-11-03 22:26:00 -0500

    Creating User Router

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -22,6 +24,9 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
